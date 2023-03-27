0e38c4b4d1093a4e4df57c831b7c9bb356adcc4b
Sansfrisk666 <126812136+Sansfrisk666@users.noreply.github.com>
2023-03-27 17:01:56 +0000

    7.3

---
+++ 7.1-arrays-intro.js
@@ -0,0 +1,25 @@
+function example() {
+  var topStudents = ["Christian", "kristian", "shakka", "Jaime", "AJ" ];
+  var name = topStudents[2];
+  console.log(name);
+  topStudents[0] = "Mauricio";
+  console.log(topStudents);
+  topStudents = "Mauricio";
+  console.log(topStudents);
+}
+
+function problem1() {
+  var topPlaces = ["californa", "hawii", "Ice land", "Mexico", "New york" ];
+  var place = topPlaces[2];
+  console.log(place);
+  topPlaces[0] = "london";
+  console.log(topPlaces);
+  topPlaces = "london";
+  console.log(topPlaces);
+  alert (place)
+}
+
+
+function problem2(){
+  
+}
